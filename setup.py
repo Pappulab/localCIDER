b06803b18a2eedb728ee146c0727c3613d79bdb3
alexholehouse <alex.holehouse@gmail.com>
2014-08-08 19:17:14 -0500

    V0.1.0 PIP version

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='localCIDER',
+    name='localcider',
     version='0.1.0',
     author='Alex Holehouse',
     author_email='alex.holehouse@wustl.edu',
-    packages=['localCIDER', 'localCIDER.test, localCIDER.kappa, localCIDER.wl, localCIDER.backend'],
-    scripts=[''],
-    url='http://pypi.python.org/pypi/TowelStuff/',
+    packages=['localcider', 'localcider.tests', 'localcider.backend', 'localcider.backend.data'],
+    scripts=[],
+    url='http://pappulab.github.io/localCIDER/',
     license='LICENSE.txt',
-    description='Tools for calculating sequence properties of disodered proteins',
+    description='Tools for calculating sequence properties of disordered proteins [from the Pappu Lab in St. Louis]',
     long_description=open('README.txt').read(),
     install_requires=[
         "numpy","matplotlib"],
-    test_suite="tests",
 )
